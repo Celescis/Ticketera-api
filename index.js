ddd1d792801099af0dd9aecb26a59f17005d2ce6
Celeste <celestecisternas@gmail.com>
2023-11-24 22:50:29 -0300

    config

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
-const mongoose = require('mongoose');
-
 const ticketRoutes = require('./src/routes/ticketRoutes');
-const ticketController = require('./src/controllers/ticketController');
+
+const connect = require('./connectMongo');
+connect();
+
+const app = express();
 require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 
-// Conectar a MongoDB local
-// mongoose.connect('mongodb://localhost:27017/Ticketera', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// }).then(() => console.log('MongoDB se conecto!'))
-//   .catch(err => console.log('No se pudo conectar', err));
-
-// Conectar a MongoDB Atlas
-mongoose.connect(process.env.MONGODB_CONNECT_URI)
-  .then(() => console.log('MongoDB se conecto!'))
-  .catch(err => console.log('No se pudo conectar', err));
-
-
 // Usar rutas
 app.use('/api/tickets', ticketRoutes);
-app.get('/', ticketController.traerTickets);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
