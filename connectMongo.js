92d8756df5062f44fe6f3d9369d4880be044a023
Celeste <celestecisternas@gmail.com>
2023-11-25 00:27:54 -0300

    arreglo

--- connectMongo.js
+++ connectMongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect(process.env.MONGODB_URI || environment.URI)
         console.log('MongoDB se conecto!')
     } catch (error) {
         console.log('No se pudo conectar' + error.message)
